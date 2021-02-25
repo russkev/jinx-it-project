6efff2f5c24c468c4c0518fcc8385aa18efd1334
russkev <kev_kix@hotmail.com>
2021-02-26 07:53:57 +1100

    Print BASE_DIR

--- src/backend/jinx_project/settings.py
+++ src/backend/jinx_project/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
+print("BASE_DIR:")
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
