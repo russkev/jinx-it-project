a46991d3de20dfe3706008660b45bdb2e9131288
russkev <kev_kix@hotmail.com>
2021-03-08 23:07:19 +1100

    Removed static stuff from settings.py

--- src/backend/jinx_project/settings.py
+++ src/backend/jinx_project/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
@@ -157,7 +157,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # User uploaded file
